d7e0583dcb4c5817a7e1e384cd1b9cf5466c5cf5
Ted Shaffer <shaffer.family@gmail.com>
2024-08-06 05:14:54 -0700

    Properly parse command line options.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,33 @@
+import commandLineArgs from 'command-line-args';
+import { isArray, isNil } from 'lodash';
+
+const optionDefinitions = [
+  { name: 'job', alias: 'j', type: String },
+  { name: 'parameters', type: String, multiple: true },
+]
+
+const options = commandLineArgs(optionDefinitions)
+console.log(options);
+
 async function main() {
   console.log('eat more pizza');
+}
+
+const getCommandLineArguments = (options: any) => {
+  if (isNil(options.job)) {
+    debugger;
+  }
+  const parameters: string[] = [];
+  if (isArray(options.parameters)) {
+    for (const parameter of options.parameters) {
+      parameters.push(parameter);
+    }
+  }
+
+  return {
+    job: options.job,
+    parameters
+  };
 }
 
 main();
